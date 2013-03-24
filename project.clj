d26615918739f4a91d2760de33f49afe740a27d9
Dave Snowdon <dave@davesnowdon.com>
2013-03-24 11:33:43 +0000

    Fixed project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject naojure "0.0.1-SNAPSHOT"
-  :description "Clojure wrapper for Aldebaran Robotics java NAOQI binding. Depends on the Aldebaran jar file being installed in a local repo and the shared library being in the dynamic library load path"
+  :description "Clojure wrapper for Aldebaran Robotics java NAOQI binding. Depends on the Aldebaran jar file and native dependencies being installed in a local maven repo"
   :url "https://github.com/davesnowdon/naojure"
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [[org.clojure/clojure "1.4.0"]
