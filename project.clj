99eeb64608afea77303f9b68ef4d68ff0e16c206
Dave Snowdon <dave@davesnowdon.com>
2013-12-01 09:36:40 +0000

    Moving files to allow for mixed clojure / java project

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject naojure "0.0.1-SNAPSHOT"
   :description "Clojure wrapper for Aldebaran Robotics java NAOQI binding. Depends on the Aldebaran jar file and native dependencies being installed in a local maven repo"
   :url "https://github.com/davesnowdon/naojure"
+  :source-paths ["src" "src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :test-paths ["test" "src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [com.aldebaran/qimessaging "1.22.1"]])
